4a4f2a146bdf686f03b801ed1e75c35435c5b5a3
DiegoArias1234 <diegoarias245794@correo.itm.edu.co>
2023-02-22 17:49:14 -0500

    Begin countries API controller

--- Sales.API/Program.cs
+++ Sales.API/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name = DockerConnection"));
+builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name = LocalConnection"));
 
 var app = builder.Build();
 
