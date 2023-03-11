4205516158eeac5c0b95554b58d58aa6cef1266d
DiegoArias1234 <diegoarias245794@correo.itm.edu.co>
2023-03-11 14:52:30 -0500

    Security in DB

--- Sales.Shared/Entities/City.cs
+++ Sales.Shared/Entities/City.cs
@@ -14,5 +14,9 @@ namespace Sales.Shared.Entities
         public int StateId { get; set; }
 
         public State? State { get; set; }
+
+        public ICollection<User>? Users { get; set; }
+
+
     }
 }
